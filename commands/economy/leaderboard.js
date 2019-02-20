4511d9ebdc5714862077ea3fb433f2ebaa281a34
Phoenix1128 <pdinsdale1128@gmail.com>
2019-02-20 17:43:14 -0500

    Fixed up leaderboard

--- commands/economy/leaderboard.js
+++ commands/economy/leaderboard.js
@@ -28,16 +28,16 @@ module.exports = { name: 'leaderboard', description: 'Shows the top 10 users on 
 
           message.channel.send(`__**${client.user}'s Economy Leaderboard:**__
    
-  **1 - ${firstplace && firstplace.tag || 'Nobody Yet'} : Balance - ${users[0] && users[0].balance || 'None'}
-  2 - ${secondplace && secondplace.tag || 'Nobody Yet'} : Balance - ${users[1] && users[1].balance || 'None'}
-  3 - ${thirdplace && thirdplace.tag || 'Nobody Yet'} : Balance - ${users[2] && users[2].balance || 'None'}
-  4 - ${thirdplace && fourthplace.tag || 'Nobody Yet'} : Balance - ${users[3] && users[3].balance || 'None'}
-  5 - ${thirdplace && fifthplace.tag || 'Nobody Yet'} : Balance - ${users[4] && users[4].balance || 'None'}
-  6 - ${thirdplace && sixthplace.tag || 'Nobody Yet'} : Balance - ${users[5] && users[5].balance || 'None'}
-  7 - ${thirdplace && seventhplace.tag || 'Nobody Yet'} : Balance - ${users[6] && users[6].balance || 'None'}
-  8 - ${thirdplace && eighthplace.tag || 'Nobody Yet'} : Balance - ${users[7] && users[7].balance || 'None'}
-  9 - ${thirdplace && ninethplace.tag || 'Nobody Yet'} : Balance - ${users[8] && users[8].balance || 'None'}
-  10 - ${thirdplace && tenthplace.tag || 'Nobody Yet'} : Balance - ${users[9] && users[9].balance || 'None'}**`);
+  **1 -** \`${firstplace && firstplace.tag || 'Nobody Yet'}\` : **Balance -** \`${users[0] && users[0].balance || 'None'}\`
+  **2 -** \`${secondplace && secondplace.tag || 'Nobody Yet'}\` : **Balance -** \`${users[1] && users[1].balance || 'None'}\`
+  **3 -** \`${thirdplace && thirdplace.tag || 'Nobody Yet'}\` : **Balance -** \`${users[2] && users[2].balance || 'None'}\`
+  **4 -** \`${fourthplace && fourthplace.tag || 'Nobody Yet'}\` : **Balance -** \`${users[3] && users[3].balance || 'None'}\`
+  **5 -** \`${fifthplace && fifthplace.tag || 'Nobody Yet'}\` : **Balance -** \`${users[4] && users[4].balance || 'None'}\`
+  **6 -** \`${sixthplace && sixthplace.tag || 'Nobody Yet'}\` : **Balance -** \`${users[5] && users[5].balance || 'None'}\`
+  **7 -** \`${seventhplace && seventhplace.tag || 'Nobody Yet'}\` : **Balance -** \`${users[6] && users[6].balance || 'None'}\`
+  **8 -** \`${eighthplace && eighthplace.tag || 'Nobody Yet'}\` : **Balance -** \`${users[7] && users[7].balance || 'None'}\`
+  **9 -** \`${ninethplace && ninethplace.tag || 'Nobody Yet'}\` : **Balance -** \`${users[8] && users[8].balance || 'None'}\`
+  **10 -** \`${tenthplace && tenthplace.tag || 'Nobody Yet'}\` : **Balance -** \`${users[9] && users[9].balance || 'None'}\``);
    
         });
    
