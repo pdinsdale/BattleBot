3d4b7709d8467e09c29af32aa6c3c6af11f8bc02
Phoenix1128 <pdinsdale1128@gmail.com>
2019-02-20 16:07:20 -0500

    Spruced up wizardfrog cmd and added more quotes

--- commands/fun/wizardfrog.js
+++ commands/fun/wizardfrog.js
@@ -1,58 +1,88 @@
 module.exports = { name: 'wizardfrog', description: 'Sends a random Wizardfrog quote', aliases: ['wf'], usage: ' ', modonly: false, async run(client, message, args, Discord) {
 
-    let random = Math.floor(Math.random() * 15);
+    let random = Math.floor(Math.random() * 17);
+
+    const embed = new Discord.RichEmbed()
+        .setAuthor('Wizardfrog Quote')
+        .setTimestamp()
+        .setFooter(`Created and Maintained by Phoenix#0408 | ${client.version}`, client.user.displayAvatarURL)
+        .setColor('#4199c2')
 
     switch(random) {
         case 0:
-        message.channel.send({file: "./images/wizardfrog_quotes/image0.jpg"});
+        embed.setImage("https://i.imgur.com/H6Z7L7F.jpg");
+        message.channel.send(embed);
         break;
         case 1:
-        message.channel.send({file: "./images/wizardfrog_quotes/image0(1).jpg"});
+        embed.setImage("https://i.imgur.com/dTMnqrN.jpg");
+        message.channel.send(embed);
         break;
         case 2:
-        message.channel.send({file: "./images/wizardfrog_quotes/image0(2).jpg"});
+        embed.setImage("https://i.imgur.com/tyVsYKO.jpg");
+        message.channel.send(embed);
         break;
         case 3:
-        message.channel.send({file: "./images/wizardfrog_quotes/image0(3).jpg"});
+        embed.setImage("https://i.imgur.com/G9AFP53.jpg");
+        message.channel.send(embed);
         break;
         case 4:
-        message.channel.send({file: "./images/wizardfrog_quotes/image0(4).jpg"});
+        embed.setImage("https://i.imgur.com/tvLAHdi.jpg");
+        message.channel.send(embed);
         break;
         case 5:
-        message.channel.send({file: "./images/wizardfrog_quotes/image0(5).jpg"});
+        embed.setImage("https://i.imgur.com/8UnqQJT.jpg");
+        message.channel.send(embed);
         break;
         case 6:
-        message.channel.send({file: "./images/wizardfrog_quotes/image0(6).jpg"});
+        embed.setImage("https://i.imgur.com/8VuHtJL.jpg");
+        message.channel.send(embed);
         break;
         case 7:
-        message.channel.send({file: "./images/wizardfrog_quotes/image0(7).jpg"});
+        embed.setImage("https://i.imgur.com/optTQS7.jpg");
+        message.channel.send(embed);
         break;
         case 8:
-        message.channel.send({file: "./images/wizardfrog_quotes/image0(8).jpg"});
+        embed.setImage("https://i.imgur.com/A0o9FKY.jpg");
+        message.channel.send(embed);
         break;
         case 9:
-        message.channel.send({file: "./images/wizardfrog_quotes/image0(9).jpg"});
+        embed.setImage("https://i.imgur.com/JHM2COl.jpg");
+        message.channel.send(embed);
         break;
         case 10:
-        message.channel.send({file: "./images/wizardfrog_quotes/image0(10).jpg"});
+        embed.setImage("https://i.imgur.com/WHSz9vu.jpg");
+        message.channel.send(embed);
         break;
         case 11:
-        message.channel.send({file: "./images/wizardfrog_quotes/image0(11).jpg"});
+        embed.setImage("https://i.imgur.com/VkWI4Yn.jpg");
+        message.channel.send(embed);
         break;
         case 12:
-        message.channel.send({file: "./images/wizardfrog_quotes/image0(12).jpg"});
+        embed.setImage("https://i.imgur.com/78gyTFL.jpg");
+        message.channel.send(embed);
         break;
         case 13:
-        message.channel.send({file: "./images/wizardfrog_quotes/image0(13).jpg"});
+        embed.setImage("https://i.imgur.com/hDw04NI.jpg");
+        message.channel.send(embed);
         break;
         case 14:
-        message.channel.send({file: "./images/wizardfrog_quotes/image0(14).jpg"});
+        embed.setImage("https://i.imgur.com/UGcbSas.jpg");
+        message.channel.send(embed);
         break;
         case 15:
-        message.channel.send({file: "./images/wizardfrog_quotes/image0(15).jpg"});
+        embed.setImage("https://i.imgur.com/fmdRorh.jpg");
+        message.channel.send(embed);
         break;
         case 16:
-        message.channel.send({file: "./images/wizardfrog_quotes/image0(16).jpg"});
+        embed.setImage("https://i.imgur.com/89rAYRr.jpg");
+        message.channel.send(embed);
+        break;
+        case 17:
+        embed.setImage("https://i.imgur.com/XInZFnY.jpg");
+        message.channel.send(embed);
+        break;
+        case 18:
+        embed.setImage("https://i.imgur.com/UakzPFf.jpg");
         break;
     }
 }};
