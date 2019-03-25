4e1494433caa3e7c1d59cda8e946a0be29dd103d
Phoenix1128 <pdinsdale1128@gmail.com>
2019-03-24 21:55:47 -0400

    Fixed reload cmd

--- commands/system/reload.js
+++ commands/system/reload.js
@@ -1,3 +1,5 @@
+/* eslint-disable global-require */
+/* eslint-disable import/no-dynamic-require */
 module.exports = {
   name: 'reload',
   description: 'Deletes and reloads the cache of the specified command',
@@ -5,64 +7,23 @@ module.exports = {
   args: '[Cmd Folder => The folder of the command \n[Cmd Name] => Name of the command',
   owneronly: true,
   async run(client, message, args) {
-    // MY COMMAND... Doesn't work and I'm too lazy to fix it
-
     const cmdFolder = args[0];
+    const cmdName = args[1];
+    const props = require(`../../commands/${cmdFolder}/${cmdName}.js`);
 
-    switch (cmdFolder.toLowerCase()) {
-      default:
-        message.reply('You screwed up you idiot!');
-        break;
-      case 'factions':
-        try {
-          delete require.cache[require.resolve(`../commands/factions/${args[1]}.js`)];
-        } catch (e) {
-          return message.channel.send(`Unable to reload: ${args[1]}`);
-        } break;
-      case 'fun':
-        try {
-          delete require.cache[require.resolve(`../commands/fun/${args[1]}.js`)];
-        } catch (e) {
-          return message.channel.send(`Unable to reload: ${args[1]}`);
-        } break;
-      case 'help':
-        try {
-          delete require.cache[require.resolve(`../commands/help/${args[1]}.js`)];
-        } catch (e) {
-          return message.channel.send(`Unable to reload: ${args[1]}`);
-        } break;
-      case 'info':
-        try {
-          delete require.cache[require.resolve(`../commands/info/${args[1]}.js`)];
-        } catch (e) {
-          return message.channel.send(`Unable to reload: ${args[1]}`);
-        } break;
-      case 'moderation':
-        try {
-          delete require.cache[require.resolve(`../commands/moderation/${args[1]}.js`)];
-        } catch (e) {
-          return message.channel.send(`Unable to reload: ${args[1]}`);
-        } break;
-      case 'system':
-        try {
-          delete require.cache[require.resolve(`../commands/system/${args[1]}.js`)];
-        } catch (e) {
-          return message.channel.send(`Unable to reload: ${args[1]}`);
-        } break;
-      case 'roles':
-        try {
-          delete require.cache[require.resolve(`../commands/roles/${args[1]}.js`)];
-        } catch (e) {
-          return message.channel.send(`Unable to reload: ${args[1]}`);
-        } break;
-      case 'economy':
-        try {
-          delete require.cache[require.resolve(`../commands/economy/${args[1]}.js`)];
-        } catch (e) {
-          return message.channel.send(`Unable to reload: ${args[1]}`);
-        } break;
+    function reload() {
+      delete require.cache[require.resolve(`../../commands/${cmdFolder}/${cmdName}.js`)];
+
+      console.log(`Attempting to reload command ${cmdName}`);
+      return client.commands.set(cmdName, props);
     }
 
-    return message.reply(`Successfully reloaded \`${args[1]}.js\``);
+    try {
+      reload();
+    } catch (e) {
+      return message.reply(`Unable to reload command \`${cmdName}\``);
+    }
+
+    return message.reply(`Successfully reloaded \`${cmdName}\``);
   },
 };
